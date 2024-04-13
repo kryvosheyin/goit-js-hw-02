ab0d76309dafd9b8623b286e0e899bb3b39e1761
kryvosheyin <alex.kryvosheyin@gmail.com>
2024-04-13 15:27:27 +0100

    homework 02 completed

---
+++ js/task-4.js
@@ -0,0 +1,30 @@
+const getShippingCost = (country) => {
+    let shippingCost = 0;
+    switch (country) {
+        case "China":
+            shippingCost = 100;
+            break;
+        case "Chile":
+            shippingCost = 250;
+            break;
+        case "Australia":
+            shippingCost = 170;
+            break;
+        case "Jamaica":
+            shippingCost = 120;
+            break;
+        default:
+            break;
+    }
+
+    return shippingCost === 0
+        ? "Sorry, there is no delivery to your country"
+        : `Shipping to ${country} will cost ${shippingCost} credits`;
+};
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
